247d2698112f6b06fea5fb8bcfb0c5e6aa79ecfe
chrr1 <adhikchris123@gmail.com>
2026-05-13 23:47:20 +0700

    setup nativewind + routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
+import { Link } from "expo-router";
 import "@/global.css"
 import { Text, View } from "react-native";
  
-export default function App() {
+export default function App() { 
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go Ondboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go SignIn</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go SignUp</Link>
     </View>
   );
 }
