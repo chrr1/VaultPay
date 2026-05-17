5ec9dd9b2400c86d9ead11c23ca76c8550d24bdf
chrr1 <adhikchris123@gmail.com>
2026-05-18 01:20:05 +0700

    home screen (balance label)

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,18 +1,38 @@
-import { Link } from "expo-router";
 import "@/global.css"
-import { Text, View } from "react-native";
-import {styled} from "nativewind"
-import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
- 
+import { Text, View, Image } from "react-native";
+import { styled } from "nativewind"
+import images from "@/constants/images"
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+
 const SafeAreaView = styled(RNSafeAreaView)
-export default function App() { 
+export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold">Home</Text>
-      
-      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go Ondboarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go SignIn</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go SignUp</Link>
+      <View className="home-header">
+        <Image source={images.avatar} className="home-avatar" />
+        <Text className="home-user-name">{HOME_USER.name}</Text>
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
+
+      <View className="home-balance-card">
+        <Text className="home-balance-label">
+          Balance
+        </Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format("MMM D, YYYY")}
+          </Text>
+        </View>
+      </View>
+
+
 
     </SafeAreaView>
   );
--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,4 @@
 import splathPattern from '@/assets/images/splash-pattern.png';
-import avatar from '@/assets/images/avatar.png';
+import avatar from '@/assets/images/mymy.png';
 
 export default {splathPattern, avatar};
